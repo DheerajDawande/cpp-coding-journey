5becd753b818a5054400ed6a25799057247ccc2c
Dheeraj Dawande | Xposure <159616308+DheerajDawande@users.noreply.github.com>
2026-04-01 19:57:01 +0530

    Update Day-01.cpp

--- Day-01.cpp
+++ Day-01.cpp
@@ -1,1 +1,77 @@
+# Day 1 🚀
+
+## Problem 1: Sum of Two Numbers
+
+### Code:
+```cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a,b;
+cout<<"Enter First Number : ";
+cin>>a;
+cout<<"Enter Second Number : ";
+cin>>b;
+
+cout<<"Sum of Numbers is :"<<a+b<<endl;
+return 0;
+}
+
+
+## Problem 2: Check Positive or Negative
+
+### Code:
+```cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter Number : ";
+    cin >> a;
+
+    if (a > 0) {
+        cout << "Number is: " << a << " is positive";
+    }
+    else if (a == 0) {
+        cout << "Number is: " << a << " = Zero" << endl;
+    }
+    else {
+        cout << "Number is: " << a << " is negative" << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
